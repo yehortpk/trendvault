78e4f3506fdf9b2103c11137255b2dcc347384b7
yehortpk <e2000work@gmail.com>
2025-11-06 10:05:09 -0800

    feat(youtube): create models: Region, Category, Video. Populate Region and Category with data from Youtube API

---
+++ youtube/models.py
@@ -0,0 +1,46 @@
+from django.core.files.storage import FileSystemStorage
+from django.db import models
+
+from trendvault import settings
+
+
+static_storage = FileSystemStorage(location=str(settings.STATIC_BASE_DIR))
+
+
+class Region(models.Model):
+    code = models.CharField(max_length=5, primary_key=True)
+    title = models.CharField(max_length=100)
+    thumb_path = models.ImageField(storage=static_storage, upload_to='regions/flags', null=True)
+
+    def __str__(self):
+        return f"{self.title} ({self.code})"
+
+
+class Category(models.Model):
+    id = models.IntegerField(primary_key=True)
+    title = models.CharField(max_length=100)
+
+    class Meta:
+        ordering = ["title"]
+
+    def __str__(self):
+        return self.title
+
+
+class Video(models.Model):
+    id = models.CharField(max_length=20, primary_key=True)
+    title = models.CharField(max_length=200)
+    published_at = models.DateTimeField()
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    region = models.ForeignKey(Region, on_delete=models.SET_NULL, null=True)
+
+    channel_id = models.CharField(max_length=50)
+    channel_title = models.CharField(max_length=200)
+    thumb_path = models.ImageField(upload_to='video/thumbnails/', null=True)
+
+    comments_count = models.IntegerField()
+    views_count = models.IntegerField()
+    likes_count = models.IntegerField()
+
+    def __str__(self):
+        return self.title
